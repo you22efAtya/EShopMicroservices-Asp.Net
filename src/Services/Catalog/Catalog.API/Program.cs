d0502b271555bfeeec972f73aabef23ed264363e
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-11-03 10:25:36 +0200

    Implemented Minimal Api endPoint with carter and mapster tools and Adding Di Services in Program.cs

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
