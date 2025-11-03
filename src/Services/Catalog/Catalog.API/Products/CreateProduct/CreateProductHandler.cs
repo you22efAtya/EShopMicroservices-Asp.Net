89eb92f634bfb9395de2a3defef2d6e7cf65daf6
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-11-03 08:24:16 +0200

    Using CQRS abstraction in features folder

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,27 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
 
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name,List<string> Category, string Description,string ImageFile, decimal Price)
-        : IRequest<CreateProductResult>;
+        : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            //Business logic to create product goes here
-            throw new NotImplementedException();
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+            // Here you would typically save the product to a database
+            return new CreateProductResult(Guid.NewGuid());
+
         }
     }
 }
