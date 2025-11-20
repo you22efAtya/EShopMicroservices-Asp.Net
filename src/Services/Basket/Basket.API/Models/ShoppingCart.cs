51be2986563821b72cd23575718113fab0895bd1
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-11-21 01:26:14 +0200

    Creating domain entities

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; } = default!;
+        public List<ShoppingCartItem> Items { get; set; } = new();
+        public decimal TotalPrice => Items.Sum(item => item.Price * item.Quantity);
+        
+        public ShoppingCart(string userName) 
+        {
+            UserName = userName;
+        }
+        //Required for Mapping
+        public ShoppingCart() { }
+    }
+}
