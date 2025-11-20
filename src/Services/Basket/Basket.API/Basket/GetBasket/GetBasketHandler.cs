289bec5dce83bc81d8c7035fdd56c1c5989ae1a9
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-11-21 01:55:06 +0200

    Implementing GetBasket feature handler (No database yet)

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,6 +1,15 @@
-﻿namespace Basket.API.Basket.GetBasket
+﻿
+namespace Basket.API.Basket.GetBasket
 {
-    public class GetBasketHandler
+    public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart Cart);
+    public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
     {
+        public async Task<GetBasketResult> Handle(GetBasketQuery request, CancellationToken cancellationToken)
+        {
+            //TODO: get basket from database
+            //var basket = await _repository.GetBasket(request.UserName);
+            return new GetBasketResult(new ShoppingCart("swn"));
+        }
     }
 }
