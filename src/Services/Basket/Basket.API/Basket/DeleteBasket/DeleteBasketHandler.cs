9bc5509c484d8efbafe1a73cd341ea605335f8ff
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-11-21 17:58:38 +0200

    Implementing DeleteBasket handler (No Database yet)

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,6 +1,22 @@
 ﻿namespace Basket.API.Basket.DeleteBasket
 {
-    public class DeleteBasketHandler
+    public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResponse>;
+    public record DeleteBasketResponse(bool IsSuccess);
+
+    public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
     {
+        public DeleteBasketCommandValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required.");
+        }
+    }
+    public class DeleteBasketCommandHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResponse>
+    {
+        public async Task<DeleteBasketResponse> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+        {
+            string userName = command.UserName;
+            // Here you would typically delete the shopping cart from a database or cache
+            return new DeleteBasketResponse(true);
+        }
     }
 }
--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,7 +1,4 @@
-﻿
-using FluentValidation;
-
-namespace Basket.API.Basket.StoreBasket
+﻿namespace Basket.API.Basket.StoreBasket
 {
     public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
     public record StoreBasketResult(string UserName);
