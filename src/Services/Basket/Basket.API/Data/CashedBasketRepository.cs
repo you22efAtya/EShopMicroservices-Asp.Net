8b26372dd7156ed7189b7a7790484057167f3aaa
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-11-26 06:34:51 +0200

    131. Develop CachedBasketRepository with Proxy Pattern, Decorator Pattern

---
+++ src/Services/Basket/Basket.API/Data/CashedBasketRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data
+{
+    public class CashedBasketRepository(IBasketRepository basketRepository, IDistributedCache cache) : IBasketRepository
+    {
+        public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            var cachedBasket = await cache.GetStringAsync(userName, cancellationToken);
+            if (!string.IsNullOrEmpty(cachedBasket))
+                return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket)!;
+
+            var basket = await basketRepository.GetBasket(userName, cancellationToken);
+            await cache.SetStringAsync(userName, JsonSerializer.Serialize(basket), cancellationToken);
+            return basket;
+
+        }
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            await basketRepository.StoreBasket(basket, cancellationToken);
+            await cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket), cancellationToken);
+            return basket;
+        }
+        public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            
+            await basketRepository.DeleteBasket(userName, cancellationToken);
+            await cache.RemoveAsync(userName, cancellationToken);
+            return true;
+        }
+    }
+}
