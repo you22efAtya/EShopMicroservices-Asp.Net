81516a815e13c1d73b4d8273b11dc0c5468b3c3b
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-11-30 05:12:18 +0200

    Entity Framework Core and SQLite Database Connection in Discount Microservice

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,9 +1,13 @@
+using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<DiscountContext>(opts =>
+        opts.UseSqlite(builder.Configuration.GetConnectionString("Database")));
 
 var app = builder.Build();
 
