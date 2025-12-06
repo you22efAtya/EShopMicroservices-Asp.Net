4bcd07a6ca3994b5578010b1bd3b37c0caeed6b5
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-12-06 14:36:52 +0200

    Developing Domain Modeling with DDD for Ordering Microservice

---
+++ src/Services/Ordering/Ordering.Domain/Model/Customer.cs
@@ -0,0 +1,6 @@
+﻿namespace Ordering.Domain.Models;
+public class Customer : Entity<Guid>
+{
+    public string Name { get; private set; } = default!;
+    public string Email { get; private set; } = default!;
+}
