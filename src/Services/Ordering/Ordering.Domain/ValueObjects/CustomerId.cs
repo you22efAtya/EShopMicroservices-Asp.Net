7dc8eccc68dc367cd6cb5bcae8787517545a5408
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-12-06 16:15:39 +0200

    Developing DDD Rich-domain model Entity for Order Entities in Customer class

--- src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ordering.Domain.ValueObjects
+﻿namespace Ordering.Domain.ValueObjects
 {
     public record CustomerId
     {
         public Guid Value { get; }
+        private CustomerId(Guid value) => Value = value;
+        public static CustomerId Of(Guid value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            if(value == Guid.Empty)
+            {
+                throw new DomainException("CustomerId cannot be an empty GUID.");
+            }
+            return new CustomerId(value);
+
+        }
     }
 }
