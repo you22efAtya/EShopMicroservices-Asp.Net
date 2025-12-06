51ea39deab1132b9c1c0c45b13733f0405983d3e
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-12-06 17:55:34 +0200

    Developing Order Created and Updated Domain Events in Ordering Microservice

---
+++ src/Services/Ordering/Ordering.Domain/Events/OrderUpdatedEvent.cs
@@ -0,0 +1,11 @@
+﻿using Ordering.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Events
+{
+    public record OrderUpdatedEvent(Order order) : IDomainEvent;
+}
