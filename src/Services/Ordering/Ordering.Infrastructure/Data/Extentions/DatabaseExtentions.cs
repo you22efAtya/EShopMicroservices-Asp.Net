bd3b185a01897789135a1e93a0b4d52ac21a3321
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-12-09 19:41:01 +0200

    Seeding Data in SQL Server OrderDb using EF Core when App Startup

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extentions/DatabaseExtentions.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extentions/DatabaseExtentions.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Ordering.Infrastructure.Data.Extensions;
 
 namespace Ordering.Infrastructure.Data.Extentions
 {
@@ -17,6 +13,39 @@ namespace Ordering.Infrastructure.Data.Extentions
             var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
             context.Database.MigrateAsync().GetAwaiter().GetResult();
+
+            await SeedAsync(context);
+        }
+        private static async Task SeedAsync(ApplicationDbContext context)
+        {
+            await SeedCustomerAsync(context);
+            await SeedProductAsync(context);
+            await SeedOrderWithItemAsync(context);
+        }
+
+        private static async Task SeedCustomerAsync(ApplicationDbContext context)
+        {
+            if(!await context.Customers.AnyAsync())
+            {
+                await context.Customers.AddRangeAsync(InitialData.Customers);
+                await context.SaveChangesAsync();
+            }
+        }
+        private static async Task SeedProductAsync(ApplicationDbContext context)
+        {
+            if (!await context.Customers.AnyAsync())
+            {
+                await context.Customers.AddRangeAsync(InitialData.Customers);
+                await context.SaveChangesAsync();
+            }
+        }
+        private static async Task SeedOrderWithItemAsync(ApplicationDbContext context)
+        {
+            if (!await context.Customers.AnyAsync())
+            {
+                await context.Customers.AddRangeAsync(InitialData.Customers);
+                await context.SaveChangesAsync();
+            }
         }
     }
 }
--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -1,2 +1,4 @@
 ﻿global using Ordering.Infrastructure.Data;
 global using Microsoft.EntityFrameworkCore;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
