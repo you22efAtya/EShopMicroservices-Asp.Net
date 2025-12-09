eb024e8a8f76d26674969efce7296f12fdc72cb1
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-12-09 17:05:58 +0200

    Setup SQL Server DB using Docker-compose file for Multi-container Docker Env

--- src/Services/Ordering/Ordering.Domain/Events/OrderCreatedEvent.cs
+++ src/Services/Ordering/Ordering.Domain/Events/OrderCreatedEvent.cs
@@ -1,11 +1,4 @@
-﻿using Ordering.Domain.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ordering.Domain.Events
+﻿namespace Ordering.Domain.Events
 {
     public record OrderCreatedEvent(Order order) : IDomainEvent;
 }
