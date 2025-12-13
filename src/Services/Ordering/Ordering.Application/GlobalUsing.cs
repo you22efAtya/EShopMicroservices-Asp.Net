b4c1d8f11a5ed9308819cbc5f41a5e78e0440409
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-12-13 20:48:22 +0200

    Initializing Domain Event Handlers

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -5,3 +5,6 @@ global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
 global using FluentValidation;
 global using Ordering.Application.Exceptions;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
+global using Ordering.Domain.Events;
