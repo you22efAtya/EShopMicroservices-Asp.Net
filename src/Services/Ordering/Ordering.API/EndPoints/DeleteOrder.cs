d03bbb6bbe8f70e431d450696ea5501afe91f6ef
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-12-15 21:05:56 +0200

    Developing DeleteOrder Endpoint for REST APIs in Ordering.API Layer

--- src/Services/Ordering/Ordering.API/EndPoints/DeleteOrder.cs
+++ src/Services/Ordering/Ordering.API/EndPoints/DeleteOrder.cs
@@ -1,6 +1,23 @@
-﻿namespace Ordering.API.EndPoints
+﻿using Ordering.Application.Orders.Commands.DeleteOrder;
+
+namespace Ordering.API.EndPoints
 {
-    public class DeleteOrder
+    public record DeleteOrderResponse(bool IsSuccess);
+    public class DeleteOrder : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/orders/{id}", async (Guid Id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteOrderCommand(Id));
+                var response = result.Adapt<DeleteOrderResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("DeleteOrder")
+            .Produces<DeleteOrderResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Delete Order")
+            .WithDescription("Deletes an order by its ID.");
+        }
     }
 }
