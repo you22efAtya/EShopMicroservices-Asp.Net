f2959659f0689b3e6252cef19c4d48fe6628a143
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-12-21 20:05:11 +0200

    Publish OrderCreated Integration event for Order Fullfiment Microservice and Use Feature Mangament to Prevent Raising Events While Seeding

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,12 +1,21 @@
-﻿namespace Ordering.Application.Orders.EventHandlers.Domain
+﻿using MassTransit;
+using Microsoft.FeatureManagement;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain
 {
-    public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
-        : INotificationHandler<OrderCreatedEvent>
+    public class OrderCreatedEventHandler
+    (IPublishEndpoint publishEndpoint, IFeatureManager featureManager, ILogger<OrderCreatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent>
     {
-        public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+        public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
         {
-            logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType().Name);
-            return Task.CompletedTask;
+            logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+
+            if (await featureManager.IsEnabledAsync("OrderFullfilment"))
+            {
+                var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+                await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+            }
         }
     }
 }
