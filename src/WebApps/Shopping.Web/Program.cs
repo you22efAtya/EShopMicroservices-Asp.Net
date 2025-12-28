92c093038891338c5173ac3cf27aa07e65c96424
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-12-28 05:22:27 +0200

    Installing and Configure Refit HttpClientFactory Library

--- src/WebApps/Shopping.Web/Program.cs
+++ src/WebApps/Shopping.Web/Program.cs
@@ -1,9 +1,18 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddRefitClient<ICatalogService>()
+    .ConfigureHttpClient(c =>
+    {
+        c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]!);
+    });
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
