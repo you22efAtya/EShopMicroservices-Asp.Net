ce6f2508c361575329189b4c07bc927aa363a646
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-12-29 02:42:10 +0200

    Developing IBasketService Classes with Refit HttpClientFactory Library

---
+++ src/WebApps/Shopping.Web/Services/IBasketService.cs
@@ -0,0 +1,20 @@
+﻿using Shopping.Web.Models.Basket;
+using System.Net;
+
+namespace Shopping.Web.Services;
+
+public interface IBasketService
+{
+    [Get("/basket-service/basket/{userName}")]
+    Task<GetBasketResponse> GetBasket(string userName);
+
+    [Post("/basket-service/basket")]
+    Task<StoreBasketResponse> StoreBasket(StoreBasketRequest request);
+
+    [Delete("/basket-service/basket/{userName}")]
+    Task<DeleteBasketResponse> DeleteBasket(string userName);
+
+    [Post("/basket-service/basket/checkout")]
+    Task<CheckoutBasketResponse> CheckoutBasket(CheckoutBasketRequest request);
+
+}
