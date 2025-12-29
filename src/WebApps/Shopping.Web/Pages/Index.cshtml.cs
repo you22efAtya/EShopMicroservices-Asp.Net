54e9b4cb60793eca8d80d48d6e708673c9bba949
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-12-29 03:49:03 +0200

    Developing Index html Pages and cs files with Shared Partial Product pages

--- src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -7,6 +7,7 @@ namespace Shopping.Web.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        public IEnumerable<ProductModel> ProductList { get; set; } = new List<ProductModel>();
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
