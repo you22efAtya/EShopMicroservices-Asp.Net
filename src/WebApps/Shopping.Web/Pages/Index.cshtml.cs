af0c841b843eb404c2d259c41f3829deedec6f9f
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-12-30 01:57:52 +0200

    Developing Index C# Page Consume YarpApiGateway Endpoints with ICatalogService

--- src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -1,20 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
 namespace Shopping.Web.Pages
 {
-    public class IndexModel : PageModel
+    public class IndexModel(ICatalogService catalogService, ILogger<IndexModel> logger) : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
         public IEnumerable<ProductModel> ProductList { get; set; } = new List<ProductModel>();
-        public IndexModel(ILogger<IndexModel> logger)
+        public async Task<IActionResult> OnGetAsync()
         {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
+            logger.LogInformation("Getting product list for index page.");
+            var result = await catalogService.GetProducts();
+            ProductList = result.Products;
+            return Page();
 
         }
     }
