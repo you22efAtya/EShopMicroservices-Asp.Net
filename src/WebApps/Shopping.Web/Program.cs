3b21e49203ed5e2c14e813d6496336c44ca87011
you22efAtya <youssefmohamedyahoo.com9@gmail.com>
2025-12-30 05:42:37 +0200

    Containerize and Orchestrate Shopping.Web Application in Docker-Compose

--- src/WebApps/Shopping.Web/Program.cs
+++ src/WebApps/Shopping.Web/Program.cs
@@ -17,6 +17,14 @@ builder.Services.AddRefitClient<IBasketService>()
         c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]!);
     });
 
+builder.Services.AddRefitClient<IOrderingService>()
+    .ConfigureHttpClient(c =>
+    {
+        c.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayAddress"]!);
+    });
+
+
+
 var app = builder.Build();
 
 
